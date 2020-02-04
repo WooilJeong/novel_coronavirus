9558d40c73dc7a0a670ed279ab72abfdc950ede2
wooil <mcwooil2@gmail.com>
2020-02-04 16:35:15 +0900

    '0204'

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ import os
 def job():
     try:
         os.system('python update_dataset.py')
+        os.system('python update_plots.py')
     except:
         os.system('python3 update_dataset.py')
+        os.system('python3 update_plots.py')
 
 # 5초에 한번씩 실행
 # schedule.every(5).seconds.do(job)
 # # 10분에 한번씩 실행
-schedule.every(3).minutes.do(job)
+# schedule.every(3).minutes.do(job)
 # # 매 시간 실행
 # schedule.every(12).hour.do(job)
 # # 매일 10:30 에 실행
-# schedule.every().day.at("10:30").do(job)
+schedule.every().day.at("10:00").do(job)
 # # 매주 월요일 실행
 # schedule.every().monday.do(job)
 # # 매주 수요일 13:15 에 실행
@@ -23,4 +25,4 @@ schedule.every(3).minutes.do(job)
 
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(5)
